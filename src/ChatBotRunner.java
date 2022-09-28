f35448af52381bc294e8e51f935dd1c9f6f96161
student <rshefa27@gmail.com>
2022-09-28 12:22:37 -0400

    Initial commit

---
+++ src/ChatBotRunner.java
@@ -0,0 +1,19 @@
+public class ChatBotRunner {
+    public static void main(String[] args) {
+        ChatBot chatBot1 = new ChatBot("Garfield", 10);
+
+        chatBot1.greeting("Mr. Fishington");
+        chatBot1.weather();
+
+        double meters = chatBot1.convertFeetToMeters(56);
+        System.out.println("Did you know? (This is a false fact) I once ate a chicken " + meters + " meters wide!");
+
+        chatBot1.favoriteNumber(611);
+
+        int nums = chatBot1.addNumbers(4,5,6);
+        System.out.println("I have around " + nums + " toothbrush(es).");
+
+        String bye = chatBot1.goodbye();
+        System.out.println(bye);
+    }
+}
---
+++ src/CircleRunner.java
@@ -0,0 +1,16 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle1 = new Circle(4);
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        // the value of double area = 3.14 * 4 * 4;
+
+        // D. run the code and confirm your prediction! Were you correct?
+        // yep
+    }
+}
